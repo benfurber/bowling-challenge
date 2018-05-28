048bc1fc6cabc9b300c1e248a82870c3c680a174
benfurber <ben@furber-consultancy.com>
2018-05-28 10:32:15 +0100

    addRoll() for end frame

--- src/__tests__/FrameStore.test.js
+++ src/__tests__/FrameStore.test.js
@@ -2,13 +2,76 @@ import React from 'react'
 import frameStore from '../stores/FrameStore'
 
 describe('FrameStore', () => {
-  describe('constructor on load', () => {
-    it('has an empty rolls array', () => {
+  describe('Constructor on load', () => {
+    it('Has an empty rolls array', () => {
       expect(frameStore._rolls).toEqual([])
     })
 
-    it('has an empty presentation array', () => {
+    it('Has an empty presentation array', () => {
       expect(frameStore._presentation).toEqual([])
+    })
+
+    it('Has a score set to zero', () => {
+      expect(frameStore._score).toEqual(0)
+    })
+
+    it('Has a type set to normal by default', () => {
+      expect(frameStore._type).toEqual('NORMAL')
+    })
+  })
+
+  describe('addRoll()', () => {
+    it('Can add a number to _rolls', () => {
+      frameStore._rolls = []
+      const number = 5
+
+      frameStore.addRoll(number)
+      expect(frameStore._rolls).toContain(number)
+    })
+
+    it('Throws an error if not given an integer', () => {
+      const notNumber = 'string'
+      expect(() => {
+        frameStore.addRoll(notNumber)
+      }).toThrow();
+    })
+  })
+
+  describe('addRoll() for a normal frame', () => {
+    beforeEach(() => {
+      frameStore._rolls = []
+      frameStore._type = 'NORMAL'
+    })
+
+    it('Throws an error if given more than two integers', () => {
+      frameStore.addRoll(5)
+      frameStore.addRoll(3)
+
+      expect(() => {
+        frameStore.addRoll(2)
+      }).toThrow();
+    })
+
+    it('Throws an error if second number plus first number would equal more than 10', () => {
+      frameStore.addRoll(5)
+
+      expect(() => {
+        frameStore.addRoll(6)
+      }).toThrow();
+    })
+  })
+
+  describe('addRoll() for an end frame', () => {
+    beforeEach(() => {
+      frameStore._rolls = []
+      frameStore._type = 'END'
+    })
+
+    it('Allows a third roll if a strike/spare has already been rolled', () => {
+      frameStore.addRoll(10)
+      frameStore.addRoll(10)
+      frameStore.addRoll(10)
+      expect(frameStore._rolls).toEqual([10, 10, 10])
     })
   })
 })
--- src/stores/FrameStore.js
+++ src/stores/FrameStore.js
@@ -1,12 +1,46 @@
 import { EventEmitter } from 'events'
 
 class FrameStore extends EventEmitter {
-  constructor() {
+  constructor(type='NORMAL') {
     super()
     this._rolls = []
     this._presentation = []
-    this.score = []
+    this._score = 0
+    this._type = type
   }
+
+  addRoll(number) {
+    this._rollChecks(number)
+    this._rolls.push(number)
+  }
+
+  _rollChecks(number) {
+    if (!Number.isInteger(number)) {
+      throw new Error("Not given an integer (whole number)")
+    }
+    if (this._type == 'NORMAL') {
+      this._rollChecksNormal(number)
+    }
+    if (this._type == 'END') {
+      this._rollChecksEnd(number)
+    }
+  }
+
+  _rollChecksNormal(number) {
+    if (this._rolls.length == 2) {
+      throw new Error("Frame already has two rolls")
+    }
+    if (this._rolls[0] + number >= 10) {
+      throw new Error("Frame can't equal more than 10")
+    }
+  }
+
+  _rollChecksEnd(number) {
+    if (this._rolls[0] + this._rolls[1] < 10) {
+      throw new Error ("Strike/spare not scored, third roll not allowed")
+    }
+  }
+
 }
 
 const frameStore = new FrameStore();
